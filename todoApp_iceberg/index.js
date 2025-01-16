be96759b3fafb8adaba97f4c018746d73b731e0c
Rishabh <rishuvisionworld@gmail.com>
2025-01-16 13:59:05 +0530

    display edit

--- todoApp_iceberg/index.js
+++ todoApp_iceberg/index.js
@@ -43,64 +43,62 @@ btn.addEventListener("click", function () {
     todoItem.innerHTML = `<span id='data'> ${todoValue} </span> <button id='cross'> X </button> <button id='done'> ✔ </button> <button id='edit'>Edit </button>`
     display.appendChild(todoItem);
 
-    display.addEventListener('click', function (e) {
-        if (e.target.getAttribute('id') == 'cross') {
-            e.target.parentNode.remove()
+})
+
+display.addEventListener('click', function (e) {
+    if (e.target.getAttribute('id') == 'cross') {
+        e.target.parentNode.remove()
+    }
+
+    if (e.target.getAttribute('id') == 'done') {
+        // if (e.target.parentNode.classList.contains('donecut')) {
+        //     e.target.parentNode.classList.remove("donecut")
+        //     e.target.parentNode.style.backgroundColor = "red"
+        // }
+
+        // else {
+        //     e.target.parentNode.classList.add("donecut");
+        //     console.log(e.target.parentNode);
+        //     e.target.parentNode.style.backgroundColor = "green"
+
+        // }
+
+        let textdata = e.target.parentNode.querySelector("#data");
+
+        textdata.style.textDecoration =  reverse_(textdata.style.textDecoration,TrueValue="line-through","");
+        e.target.parentNode.style.backgroundColor=  reverse_(e.target.parentNode.style.backgroundColor,TrueValue="green","red");
+        
+        // flag = !flag;
+        // if (flag){
+        //     textdata.style.textDecoration = "line-through";
+        //      e.target.parentNode.style.backgroundColor = "green"
+        //      console.log(textdata.style.textDecoration)
+
+        // }
+        // else{
+        //     textdata.style.textDecoration =  null;
+        //     console.log(textdata.style.textDecoration=="")
+        //      e.target.parentNode.style.backgroundColor = "red"
+        // }
+        
+
+
+
+    }
+
+
+    if (e.target.getAttribute("id") == 'edit') {
+        let todoPrevValue = e.target.parentNode.querySelector("#data").textContent;
+        let newValue = prompt("Enter the new Value", todoPrevValue);
+        if (newValue != "" || newValue != null) {
+            e.target.parentNode.querySelector("#data").innerHTML = newValue
         }
 
-        if (e.target.getAttribute('id') == 'done') {
-            // if (e.target.parentNode.classList.contains('donecut')) {
-            //     e.target.parentNode.classList.remove("donecut")
-            //     e.target.parentNode.style.backgroundColor = "red"
-            // }
-
-            // else {
-            //     e.target.parentNode.classList.add("donecut");
-            //     console.log(e.target.parentNode);
-            //     e.target.parentNode.style.backgroundColor = "green"
-
-            // }
-
-            let textdata = e.target.parentNode.querySelector("#data");
-
-            textdata.style.textDecoration =  reverse_(textdata.style.textDecoration,TrueValue="line-through","");
-            e.target.parentNode.style.backgroundColor=  reverse_(e.target.parentNode.style.backgroundColor,TrueValue="green","red");
-            
-            // flag = !flag;
-            // if (flag){
-            //     textdata.style.textDecoration = "line-through";
-            //      e.target.parentNode.style.backgroundColor = "green"
-            //      console.log(textdata.style.textDecoration)
-
-            // }
-            // else{
-            //     textdata.style.textDecoration =  null;
-            //     console.log(textdata.style.textDecoration=="")
-            //      e.target.parentNode.style.backgroundColor = "red"
-            // }
-            
-
-
-
+        else {
+            alert("Please enter a non null value ")
         }
 
-
-        if (e.target.getAttribute("id") == 'edit') {
-            let todoPrevValue = e.target.parentNode.querySelector("#data").textContent;
-            let newValue = prompt("Enter the new Value", todoPrevValue);
-            if (newValue != "" || newValue != null) {
-                e.target.parentNode.querySelector("#data").innerHTML = newValue
-            }
-
-            else {
-                alert("Please enter a non null value ")
-            }
-
-        }
-    })
-
-
-
+    }
 })
 
 
